f594eb78489e0e8addcc37eb283cd11300eb809a
Jim <jmerk12086@gmail.com>
2020-08-25 16:38:06 -0400

    added currey and co base template

---
+++ template-currey.php
@@ -0,0 +1,256 @@
+<?php /* Template Name: Currey and Co. */ ?>
+
+<?php get_header(); ?>
+
+<div class="container mt-2">
+    <div class="row justify-content-center">
+
+           <div id="brand-page_heading_image" class="col-md-8">
+			  <div class="prod_cat_img">
+                <img style="height: auto; width: 100%;" src="">
+              </div>
+			</div>
+
+        <div class="col-lg-4 col-12 title-container">
+            <div class="row h-100 justify-content-center">
+                <div class="col-lg-12 col-12 my-auto">
+                    <div class="prod_cat_text" style="padding: 10px;">
+                        <h2 class="prod-cat-title">Currey & Co.</h2>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="wrapper" id="sale-page-wrapper">
+
+    <div id="content" class="container">
+
+        <div id="primary" class="content-area col-md-12">
+
+            <main id="main" class="site-main" role="main">
+
+                <div class="row mb-4 mt-4">
+
+                    <div class="col-sm-12 mb-5 pb-2" style="border-bottom: 2px solid #d3d3d3;">
+                        <h2>Currey and Co. Categories</h2>
+                    </div>
+
+                    <div class="col-sm-4 col-6 mt-5">
+
+                        <a href="<?php echo site_url('/product-category/currey-co/currey-co-home-decor/currey-co-mirrors'); ?>">
+                            <div class="brand-page_cat_image_container pt-3">
+                                <?php echo wp_image_add_srcset_and_sizes('<img style="height: auto; width: 100%;" src="">', wp_get_attachment_metadata(),  ); ?>
+								 <div class="pb-3 pt-3">
+                                    <h3 class="brand-page_cat_headings" style="text-align: center;" class="mb-2">Mirrors</h3>
+                                </div>
+                            </div>
+                        </a>
+
+                    </div>
+
+                    <div class="col-sm-4 col-6 mt-5">
+
+                        <a href="<?php echo site_url('/product-category/currey-co/currey-co-home-decor/currey-co-accessories/currey-co-sculptures-and-figurines'); ?>">
+                            <div class="brand-page_cat_image_container pt-3">
+                                <?php echo wp_image_add_srcset_and_sizes('<img style="height: auto; width: 100%;" src="">', wp_get_attachment_metadata(), ); ?>
+                                <div class="pb-3 pt-3">
+                                    <h3 class="brand-page_cat_headings" style="text-align: center;" class="mb-2">Sculptures and Figurines</h3>
+                                </div>
+                            </div>
+                        </a>
+
+                    </div>
+
+                    <div class="col-sm-4 col-6 mt-5">
+
+                        <a href="<?php echo site_url('/product-category/currey-co/currey-co-home-decor/currey-co-accessories/currey-co-bowls-and-trays'); ?>">
+                            <div class="brand-page_cat_image_container pt-3">
+                                <?php echo wp_image_add_srcset_and_sizes('<img style="height: auto; width: 100%;" src="">', wp_get_attachment_metadata(),  ); ?>
+                                <div class="pb-3 pt-3">
+                                    <h3 class="brand-page_cat_headings" style="text-align: center;" class="mb-2">Decorative Bowls and Trays</h3>
+                                </div>
+                            </div>
+                        </a>
+
+                    </div>
+
+                    <div class="col-sm-4 col-6 mt-5">
+
+                        <a href="<?php echo site_url('/product-category/currey-co/currey-co-home-decor/currey-co-accessories/currey-co-boxes-and-containers'); ?>">
+                            <div class="brand-page_cat_image_container pt-3">
+                                <?php echo wp_image_add_srcset_and_sizes('<img style="height: auto; width: 100%;" src="">', wp_get_attachment_metadata(),  ); ?>
+                                <div class="pb-3 pt-3">
+                                    <h3 class="brand-page_cat_headings" style="text-align: center;" class="mb-2">Decorative Boxes and Containers</h3>
+                                </div>
+                            </div>
+                        </a>
+
+                    </div>
+
+                    <div class="col-sm-4 col-6 mt-5">
+
+                        <a href="<?php echo site_url('/product-category/currey-co/currey-co-home-decor/currey-co-accessories/currey-co-vases-and-bottles'); ?>">
+                            <div class="brand-page_cat_image_container pt-3">
+                                <?php echo wp_image_add_srcset_and_sizes('<img style="height: auto; width: 100%;" src="">', wp_get_attachment_metadata(),  ); ?>
+                                <div class="pb-3 pt-3">
+                                    <h3 class="brand-page_cat_headings" style="text-align: center;" class="mb-2">Decorative Vases and Bottles</h3>
+                                </div>
+                            </div>
+                        </a>
+
+                    </div>
+
+					<div class="col-sm-4 col-6 mt-5">
+
+                        <a href="<?php echo site_url('/product-category/currey-co/currey-co-home-decor/'); ?>">
+                            <div class="brand-page_cat_image_container pt-3">
+                                <?php echo wp_image_add_srcset_and_sizes('<img style="height: auto; width: 100%;" src="">', wp_get_attachment_metadata(),  ); ?>
+                                <div class="pb-3 pt-3">
+                                    <h3 class="brand-page_cat_headings" style="text-align: center;" class="mb-2">All Decor</h3>
+                                </div>
+                            </div>
+                        </a>
+
+                    </div>
+
+                    <div class="col-sm-4 col-6 mt-5">
+
+                        <a href="<?php echo site_url('/product-category/currey-co-home-decor/currey-co-accessories/'); ?>">
+                            <div class="brand-page_cat_image_container pt-3">
+                                <?php echo wp_image_add_srcset_and_sizes('<img style="height: auto; width: 100%;" src="">', wp_get_attachment_metadata(), ); ?>
+                                <div class="pb-3 pt-3">
+                                    <h3 class="brand-page_cat_headings" style="text-align: center;" class="mb-2">All Accessories</h3>
+                                </div>
+                            </div>
+                        </a>
+
+                    </div>
+
+					<div class="col-sm-4 col-6 mt-5">
+
+                        <a href="<?php echo site_url('/product-category/currey-co/currey-co-lighting/currey-co-lamps/currey-co-table-lamps'); ?>">
+                            <div class="brand-page_cat_image_container pt-3">
+                                <?php echo wp_image_add_srcset_and_sizes('<img style="height: auto; width: 100%;" src="">', wp_get_attachment_metadata(), ); ?>
+								 <div class="pb-3 pt-3">
+                                    <h3 class="brand-page_cat_headings" style="text-align: center;" class="mb-2">Table Lamps</h3>
+                                </div>
+                            </div>
+                        </a>
+
+                    </div>
+
+					<div class="col-sm-4 col-6 mt-5">
+
+                        <a href="<?php echo site_url('/product-category/currey-co/currey-co-lighting/currey-co-lamps/currey-co-floor-lamps'); ?>">
+                            <div class="brand-page_cat_image_container pt-3">
+                                <?php echo wp_image_add_srcset_and_sizes('<img style="height: auto; width: 100%;" src="">', wp_get_attachment_metadata(),  ); ?>
+								 <div class="pb-3 pt-3">
+                                    <h3 class="brand-page_cat_headings" style="text-align: center;" class="mb-2">Floor Lamps</h3>
+                                </div>
+                            </div>
+                        </a>
+
+                    </div>
+
+                    <div class="col-sm-4 col-6 mt-5">
+
+                        <a href="<?php echo site_url('/product-category/currey-co/currey-co-lighting/currey-co-pendants'); ?>">
+                            <div class="brand-page_cat_image_container pt-3">
+                                <?php echo wp_image_add_srcset_and_sizes('<img style="height: auto; width: 100%;" src="">', wp_get_attachment_metadata(),  ); ?>
+                                <div class="pb-3 pt-3">
+                                    <h3 class="brand-page_cat_headings" style="text-align: center;" class="mb-2">Pendants</h3>
+                                </div>
+                            </div>
+                        </a>
+
+                    </div>
+
+                    <div class="col-sm-4 col-6 mt-5">
+
+                        <a href="<?php echo site_url('/product-category/currey-co/currey-co-lighting/currey-co-chandeliers'); ?>">
+                            <div class="brand-page_cat_image_container pt-3">
+                                <?php echo wp_image_add_srcset_and_sizes('<img style="height: auto; width: 100%;" src="">', wp_get_attachment_metadata(),  ); ?>
+                                <div class="pb-3 pt-3">
+                                    <h3 class="brand-page_cat_headings" style="text-align: center;" class="mb-2">Chandeliers</h3>
+                                </div>
+                            </div>
+                        </a>
+
+                    </div>
+
+                    <div class="col-sm-4 col-6 mt-5">
+
+                        <a href="<?php echo site_url('/product-category/currey-co/currey-co-lighting/currey-co-flush-mounts'); ?>">
+                            <div class="brand-page_cat_image_container pt-3">
+                                <?php echo wp_image_add_srcset_and_sizes('<img style="height: auto; width: 100%;" src="">', wp_get_attachment_metadata(),  ); ?>
+                                <div class="pb-3 pt-3">
+                                    <h3 class="brand-page_cat_headings" style="text-align: center;" class="mb-2">Flush Mounts</h3>
+                                </div>
+                            </div>
+                        </a>
+
+                    </div>
+
+                    <div class="col-sm-4 col-6 mt-5">
+
+                        <a href="<?php echo site_url('/product-category/currey-co/currey-co-lighting/currey-co-sconces'); ?>">
+                            <div class="brand-page_cat_image_container pt-3">
+                                <?php echo wp_image_add_srcset_and_sizes('<img style="height: auto; width: 100%;" src="">', wp_get_attachment_metadata(),  ); ?>
+                                <div class="pb-3 pt-3">
+                                    <h3 class="brand-page_cat_headings" style="text-align: center;" class="mb-2">Sconces</h3>
+                                </div>
+                            </div>
+                        </a>
+
+                    </div>
+
+                    <div class="col-sm-4 col-6 mt-5">
+
+                        <a href="<?php echo site_url('/product-category/currey-co/currey-co-lighting'); ?>">
+                            <div class="brand-page_cat_image_container pt-3">
+                                <?php echo wp_image_add_srcset_and_sizes('<img style="height: auto; width: 100%;" src="">', wp_get_attachment_metadata(),  ); ?>
+                                <div class="pb-3 pt-3">
+                                    <h3 class="brand-page_cat_headings" style="text-align: center;" class="mb-2">All Lighting</h3>
+                                </div>
+                            </div>
+                        </a>
+
+                    </div>
+
+                    <div class="col-md-12 mt-5 button-container" style="text-align: center;">
+                        <div class="mb-5 mt-5">
+                            <a href="<?php echo site_url('/product-category/currey-co'); ?>">
+                                <button class="button">Shop all of Currey and Co.</button>
+                            </a>
+                        </div>
+                    </div>
+
+					<div class="col-md-12 pb-2 mb-5 mt-5" style="border-bottom: 2px solid #d3d3d3;">
+                        <h2>Popular Currey and Co. Products</h2>
+                    </div>
+
+                    <div class="col-md-12">
+                        <?php echo do_shortcode('[products limit="6" columns="3" best_selling="true" category="currey-co" ]'); ?>
+                    </div>
+
+                    <div class="col-md-12 mt-3" style="border-top: 2px solid #d3d3d3;"></div>
+
+                </div>
+                <!-- end Row one -->
+
+            </main>
+            <!-- end main -->
+
+        </div>
+        <!-- end primary -->
+
+    </div>
+    <!-- end container -->
+
+</div>
+<!-- end wrapper -->
+
+<?php get_footer(); ?>
