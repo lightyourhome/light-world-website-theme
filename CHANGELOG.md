19caa350a901f85fcdec7a31a098b9710a660ed6
Jim <jmerk12086@gmail.com>
2020-10-06 11:03:52 -0400

    added kichler sale to sale page

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## Added
 
 - Kichler sale to home page
+- Kichler sale to sale page
 
 ## Removed
 
--- sale-page-et2-maxim-update.php
+++ sale-page-et2-maxim-update.php
@@ -33,6 +33,31 @@
                     <div id="sales-page-featured-sales" style="border-bottom: 2px solid #d3d3d3;" class="col-md-12 mt-3 mb-3">
                         <h1>Our Featured Sales</h1>
                     </div>
+
+                    <div class="col-md-4">
+
+                        <div class="block-picture-primary-container">
+
+                            <a href="<?php echo site_url('/brands/kichler-lighting/'); ?>">
+                                <div class="block-container">
+                                    <div class="block-picture">
+                                        <?php echo wp_image_add_srcset_and_sizes('<img style="height: auto; width: 100%;" src="/wp-content/uploads/2020/06/kichler_summer_sale_2020.jpg">', wp_get_attachment_metadata(172667), 172667 ); ?>
+                                    </div>
+                                    <div class="block-caption-wrapper">
+                                        <div class="block-caption-container">
+                                            <h4 class="block-heading">Kichler Lighting</h4>
+                                            <h3 class="block-heading">Get 15% off on All Kichler Lighting</h3>
+                                            <h4 class="block-heading">Prices already discounted!</h4>
+                                            <a style="text-align: center;" href="<?php echo site_url('/brands/kichler-lighting/'); ?>" class="block-shop-link">Shop Now</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+
+                        </div>
+
+                    </div>
+
                     
                     <div class="col-md-4">
 
