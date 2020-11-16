628fda46c2df73a21d9131c2fd39ebb370d6b2d8
Jim <jmerk12086@gmail.com>
2020-11-16 16:47:03 -0500

    added elan sale to sales page

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,17 @@ All notable theme changes should be added to this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [ released ] - 11.16.2020
+
+## [ 1.27.2 ]
+
+## Added
+
+- Elan sale to sales page
+
 ## [ released ] - 11.3.2020
+
+## [ 1.27.1 ]
 
 ## Added
 
--- sale-page-et2-maxim-update.php
+++ sale-page-et2-maxim-update.php
@@ -204,6 +204,30 @@
 
                     <div class="col-md-4">
 
+                        <div class="block-picture-primary-container">
+
+                            <div class="block-container">
+                                <a href="<?php echo site_url('/product-category/elan-lighting/elan-lighting-sale/'); ?>">
+                                    <div class="block-picture">
+                                        <?php echo wp_image_add_srcset_and_sizes('<img style="height: auto; width: 100%;" src="/wp-content/uploads/2020/01/83918_horizontal.jpg">', wp_get_attachment_metadata(110064), 110064 ); ?>
+                                    </div>
+                                    <div class="block-caption-wrapper">
+                                        <div class="block-caption-container">
+                                            <h4 class="block-heading">Élan Lighting</h4>
+                                            <h3 class="block-heading">Get 20% off on select Élan Lighting Products</h3>
+                                            <h4 class="block-heading">Prices already discounted!</h4>
+                                            <a style="text-align: center;" href="<?php echo site_url('/product-category/elan-lighting/elan-lighting-sale/'); ?>" class="block-shop-link">Shop Now</a>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="col-md-4">
+
                         <div class="block-container">
                             <a href="<?php echo site_url('/brands/elk-home'); ?>">
                                 <div class="block-picture">
