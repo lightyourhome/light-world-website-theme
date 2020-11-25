88ea6850e84258e21ab8c2c9ea6c36656059bc00
Jim <jmerk12086@gmail.com>
2020-11-25 16:01:58 -0500

    updated maxim and et2 lighting sale text

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,14 @@ All notable theme changes should be added to this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [ released ] - 11.25.2020
+
+## [ 1.28.1 ]
+
+## Changed
+
+- ET2 and Maxim Lighting Sale text
+
 ## [ released ] - 11.24.2020
 
 ## [ 1.28.0 ]
--- sale-page-et2-maxim-update.php
+++ sale-page-et2-maxim-update.php
@@ -258,7 +258,7 @@
                                     <div class="block-caption-wrapper">
                                         <div class="block-caption-container">
                                             <h4 class="block-heading">Maxim Lighting</h4>
-                                            <h3 class="block-heading">Get 15% off on All Maxim Lighting</h3>
+                                            <h3 class="block-heading">Get 20% off on All Maxim Lighting</h3>
                                             <h4 class="block-heading">Prices already discounted!</h4>
                                             <a style="text-align: center;" href="<?php echo site_url('/brands/maxim-lighting'); ?>" class="block-shop-link">Shop Now</a>
                                         </div>
@@ -282,7 +282,7 @@
                                     <div class="block-caption-wrapper">
                                         <div class="block-caption-container">
                                             <h4 class="block-heading">ET2 Lighting</h4>
-                                            <h3 class="block-heading">Get 15% off on All ET2 Lighting</h3>
+                                            <h3 class="block-heading">Get 20% off on All ET2 Lighting</h3>
                                             <h4 class="block-heading">Prices already discounted!</h4>
                                             <a style="text-align: center;" href="<?php echo site_url('/brands/et2-lighting'); ?>" class="block-shop-link">Shop Now</a>
                                         </div>
